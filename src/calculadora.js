864a6f55dcf266ad157a985133c7abed8ad97adc
Yossueth <patricksarguedas2806@gmail.com>
2024-08-16 14:02:35 -0600

    practica4

---
+++ src/calculadora.js
@@ -0,0 +1,8 @@
+import { calculadora } from "./index.js";
+
+let calcular = new calculadora(4, 5);
+
+console.log(calcular.sumar());
+console.log(calcular.restar());
+console.log(calcular.dividir());
+console.log(calcular.multiplicar());
--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-// Inserte el código aquí
+class calculadora {
+  constructor(valor1, valor2) {
+    this.valor1 = valor1;
+    this.valor2 = valor2;
+  }
 
+  sumar() {
+    return this.valor1 + this.valor2;
+  }
+
+  restar() {
+    return this.valor1 - this.valor2;
+  }
+
+  dividir() {
+    if (this.valor2 === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return this.valor1 / this.valor2;
+  }
+
+  multiplicar() {
+    return this.valor1 * this.valor2;
+  }
+}
+
+export { calculadora };
